208a36a5a5b4fd6928389867872f612f378b7ec5
Chris Cassano <chris@litprotocol.com>
2022-03-18 17:57:07 +0000

    fixed the scraper.  need to lint it when i'm not on a plane

--- utils.js
+++ utils.js
@@ -2,16 +2,21 @@ import pa11y from "pa11y";
 
 export async function testWithPa11y({url, db}) {
   console.log(`Scraping ${url}`)
+	let results
   try {
-    const results = await pa11y(url);
+    results = await pa11y(url);
   }catch(e){
     console.log('pa11y error.  swallowing', e)
   }
+	if (!results){
+		return
+	}
   // insert results
   await db.run("INSERT INTO results (domain, results) VALUES (?, ?)", [
     url,
     JSON.stringify(results),
   ]);
+	console.log(`committed ${url}`);
 }
 
 
